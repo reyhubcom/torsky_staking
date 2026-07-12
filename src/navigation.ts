408226013b1e668f47fe3023046f014049bb44e6
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-07-12 16:57:47 -0400

    1

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,7 +7,7 @@ export const headerData = {
       links: [
         {
           text: 'Bitcoin Staking',
-          href: getPermalink('/homes/saas'),
+          href: getPermalink('/home/stakeBTC'),
         },
         {
           text: 'Stake [ ETH ]',
